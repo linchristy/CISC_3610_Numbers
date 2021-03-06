2935de50c2c5ae297631f6362023cca1692d2367
linchristy <christy.lin92@myhunter.cuny.edu>
2021-03-05 21:48:08 -0500

    Added files

---
+++ animation.js
@@ -0,0 +1,92 @@
+"use strict";
+
+var Scene = {
+    canvas : undefined,
+    canvasContext : undefined,
+	sprite: undefined
+};
+
+Scene.start = function () {
+	// Get the canvas and it's context.
+    Scene.canvas = document.getElementById("myCanvas");
+    Scene.canvasContext = Scene.canvas.getContext("2d");
+	
+	// Setup the number to be displayed.
+    Scene.sprite = number;
+	
+	// Attach the image to be used for the sprite.
+	Scene.sprite.img = new Image();
+    Scene.sprite.img.src = Scene.sprite.src;
+	
+	// Wait till the number image is loaded before starting the animation.
+	Scene.sprite.img.onload = function() {		
+		Scene.sprite.offset=Scene.sprite.frames[Scene.sprite.frame].frame.w;
+    	Scene.mainLoop();
+	}
+};
+
+
+
+// Once the basic HTML document is loaded and its parsing has taken place, start the scene.
+document.addEventListener( 'DOMContentLoaded', Scene.start);
+
+Scene.clearCanvas = function () {
+    Scene.canvasContext.fillStyle = "#FFFFFF";
+    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
+};
+
+Scene.mainLoop = function() {
+	var counter =0;
+	if(counter > 10){
+		return;
+	}else if(counter == 10){
+		Scene.clearCanvas();
+		Scene.update();
+		Scene.drawTen();
+	}else{
+		Scene.clearCanvas();
+		Scene.update();
+		Scene.draw();
+	}
+
+	// Animate at 2 frames a second.
+    window.setTimeout(Scene.mainLoop, 500);
+	// Advance to the next frame.
+	Scene.sprite.frame++;
+	counter++;
+	
+};
+
+Scene.drawTen = function(){
+	Scene.canvasContext.drawImage(Scene.sprite.img,
+									Scene.sprite.frames[1].frame.x,
+									Scene.sprite.frames[1].frame.y,
+									Scene.sprite.frames[1].frame.w,
+									Scene.sprite.frames[1].frame.h,
+									20,0,
+									Scene.sprite.frames[1].frame.w,
+									Scene.sprite.frames[1].frame.h);
+	Scene.canvasContext.drawImage(Scene.sprite.img,
+									Scene.sprite.frames[0].frame.x,
+									Scene.sprite.frames[0].frame.y,
+									Scene.sprite.frames[0].frame.w,
+									Scene.sprite.frames[0].frame.h,
+									80,0,
+									Scene.sprite.frames[0].frame.w,
+									Scene.sprite.frames[0].frame.h);
+}
+
+Scene.update = function () {
+	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
+  	Scene.canvas.width = window.innerWidth;
+	
+	// Set the location of the next frame. 
+  	Scene.sprite.offset=631;
+};
+
+
+Scene.draw = function () {
+	Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[Scene.sprite.frame].frame.x,Scene.sprite.frames[Scene.sprite.frame].frame.y,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h,600,0,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h);
+};
+
+
---
+++ number.js
@@ -0,0 +1,250 @@
+number={
+	src: "spritesheet.png",
+	img: undefined,
+	frame:0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+				"x": 1035,
+				"y": 463,
+				"w": 196,
+				"h": 292
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 1035,
+				"y": 463,
+				"w": 196,
+				"h": 292
+			},
+			"sourceSize": {
+				"w": 196,
+				"h": 292
+			}
+		},
+		{
+			"filename": "tile001.png",
+			"frame": {
+				"x": 3,
+				"y": 45,
+				"w": 216,
+				"h": 326
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 3,
+				"y": 45,
+				"w": 216,
+				"h": 326
+			},
+			"sourceSize": {
+				"w": 216,
+				"h": 326
+			}
+		},
+		{
+			"filename": "tile002.png",
+			"frame": {
+				"x": 261,
+				"y": 45,
+				"w": 196,
+				"h": 324
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 261,
+				"y": 45,
+				"w": 196,
+				"h": 324
+			},
+			"sourceSize": {
+				"w": 196,
+				"h": 324
+			}
+		},
+		{
+			"filename": "tile003.png",
+			"frame": {
+				"x": 485,
+				"y": 45,
+				"w": 230,
+				"h": 324
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 485,
+				"y": 45,
+				"w": 230,
+				"h": 324
+			},
+			"sourceSize": {
+				"w": 230,
+				"h": 324
+			}
+		},
+		{
+			"filename": "tile004.png",
+			"frame": {
+				"x": 751,
+				"y": 45,
+				"w": 222,
+				"h": 324
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 751,
+				"y": 45,
+				"w": 222,
+				"h": 324
+			},
+			"sourceSize": {
+				"w": 222,
+				"h": 324
+			}
+		},
+		{
+			"filename": "tile005.png",
+			"frame": {
+				"x": 35,
+				"y": 448,
+				"w": 196,
+				"h": 324
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 35,
+				"y": 448,
+				"w": 196,
+				"h": 324
+			},
+			"sourceSize": {
+				"w": 196,
+				"h": 324
+			}
+		},
+		{
+			"filename": "tile006.png",
+			"frame": {
+				"x": 253,
+				"y": 463,
+				"w": 246,
+				"h": 292
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 253,
+				"y": 463,
+				"w": 246,
+				"h": 292
+			},
+			"sourceSize": {
+				"w": 246,
+				"h": 292
+			}
+		},
+		{
+			"filename": "tile007.png",
+			"frame": {
+				"x": 477,
+				"y": 463,
+				"w": 198,
+				"h": 324
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 477,
+				"y": 463,
+				"w": 198,
+				"h": 324
+			},
+			"sourceSize": {
+				"w": 198,
+				"h": 324
+			}
+		},
+		{
+			"filename": "tile008.png",
+			"frame": {
+				"x": 735,
+				"y": 463,
+				"w": 198,
+				"h": 292
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 735,
+				"y": 463,
+				"w": 198,
+				"h": 292
+			},
+			"sourceSize": {
+				"w": 198,
+				"h": 292
+			}
+		},
+		{
+			"filename": "tile009.png",
+			"frame": {
+				"x": 1027,
+				"y": 60,
+				"w": 196,
+				"h": 306
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 1027,
+				"y": 60,
+				"w": 196,
+				"h": 306
+			},
+			"sourceSize": {
+				"w": 196,
+				"h": 306
+			}
+		},
+		{
+			"filename": "tile010.png",
+			"frame": {
+				"x": 1350,
+				"y": 463,
+				"w": 250,
+				"h": 330
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 1350,
+				"y": 463,
+				"w": 250,
+				"h": 330
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 330
+			}
+		},
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "spritesheet.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 1250,
+			"h": 806
+		},
+		"scale": "1"
+	}
+}
